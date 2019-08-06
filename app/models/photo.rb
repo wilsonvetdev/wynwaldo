949d6c443c02ea33f7cdafb89f27bea2d2d6176f
Andy Weiss <andyweiss1982@gmail.com>
2019-08-06 19:25:49 -0400

    move coordinate harvesting to before_create

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,11 +1,12 @@
 class Photo < ApplicationRecord
   has_one_attached :image
 
-  def pull_coords_from_ar_metadata
+  before_create :pull_coords_from_image_metadata
+
+  def pull_coords_from_image_metadata
     if image.attached?
-      self.latitude = image.blob.metadata["latitude"]
-      self.longitude = image.blob.metadata["longitude"]
-      self.save!
-    end    
+      self.latitude   = image.blob.metadata["latitude"]
+      self.longitude  = image.blob.metadata["longitude"]
+    end
   end
 end
