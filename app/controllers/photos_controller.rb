ab3acff3d283cadff74fbbec554815d88d0477b9
Andy Weiss <andyweiss1982@gmail.com>
2019-08-06 19:26:56 -0400

    manually validate and display notice or alert -- model validations are useless because of the order in which things happen

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -9,8 +9,14 @@ class PhotosController < ApplicationController
   end
 
   def create
-    photo = Photo.create!(photo_params)
-    redirect_to photo_path(photo.id)
+    photo = Photo.create(photo_params)
+    photo.image.blob.analyze
+    if photo.image.blob.metadata["latitude"] && photo.image.blob.metadata["longitude"]
+      redirect_to photo, notice: "Photo created!"
+    else
+      photo.destroy
+      redirect_to photos_url, alert: "Sorry, we couldn't determine the location of that photo."
+    end
   end
 
   private
