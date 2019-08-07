05782c96cada7df98adb410d44362f152e3d7ed5
Andy Weiss <andyweiss1982@gmail.com>
2019-08-07 16:38:17 -0400

    use dropzone from npm

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,13 +7,8 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-
-const Dropzone = require("./dropzone.js")
-
-Dropzone.options.imageUploadDropzone = {
-  paramName: "photo[image]",
-  uploadMultiple: false
-}
+window.Dropzone = require("dropzone")
+Dropzone.autoDiscover = false
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
