03bb055902c7e664e588d0a103bce43f86b4fa30
Aaron Holland <aaronholla@gmail.com>
2019-08-07 12:38:12 -0400

    added basic dropzone

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,12 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+const Dropzone = require("./dropzone.js")
+
+Dropzone.options.imageUploadDropzone = {
+  paramName: "photo[image]",
+  uploadMultiple: false
+}
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
