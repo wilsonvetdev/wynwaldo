f09b5372d97558a3a7daf8e927543771d1a6bfb1
Andy Weiss <andyweiss1982@gmail.com>
2019-08-09 14:52:56 -0400

    show photos on map

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,4 +1,25 @@
 class MapsController < ApplicationController
   def show
+    respond_to do |format|
+      format.html
+      format.json do
+        @photos = Photo.all
+        render json: {
+          type: "FeatureCollection",
+          features: @photos.map do |photo|
+            {
+              type: "Feature",
+              geometry: {
+                type: "Point",
+                coordinates: [photo.longitude, photo.latitude]
+              },
+              properties: {
+                image: url_for(photo.image)
+              }
+            }
+          end
+        }
+      end
+    end
   end
 end
--- app/javascript/bundles/Map/components/Map.jsx
+++ app/javascript/bundles/Map/components/Map.jsx
@@ -3,9 +3,21 @@ import React from 'react'
 class Map extends React.Component {
   componentDidMount() {
     mapboxgl.accessToken = this.props.token
+    this.createMap()
+  }
+
+  createMap = () => {
     this.map = new mapboxgl.Map({
       container: this.mapContainer,
       style: `mapbox://styles/mapbox/streets-v9`
+    })
+    this.map.on('load', () => {
+      this.map.addSource('photos',
+      {
+        type: 'geojson',
+        data: '/map.json'
+      })
+      this.map.addLayer({ id: 'photos', type: 'circle', source: 'photos'});
     })
   }
 
