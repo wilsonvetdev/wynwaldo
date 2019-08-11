7125077ce3174cf719d72410e485852155d8ef4f
Aaron Holland <aaronholla@gmail.com>
2019-08-11 14:07:53 -0400

    updated routes to match login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
   get 'hello_world', to: 'hello_world#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'photos#index'
   resources :photos, only: [:index, :show, :create]
   resource :map, only: [:show]
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'signup'}
 end
