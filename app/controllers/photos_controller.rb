96161030419a3f2b5f0ea1003aa9632c2dda17a7
Andy Weiss <andyweiss1982@gmail.com>
2019-08-12 15:58:34 -0400

    minor changes

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,7 +1,7 @@
 class PhotosController < ApplicationController
   def index
     @photo = Photo.new
-    @photos = Photo.with_attached_image
+    @photos = Photo.with_attached_image.includes(:user)
   end
 
   def show
