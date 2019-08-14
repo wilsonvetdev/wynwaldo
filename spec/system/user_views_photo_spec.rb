963a35e3960740b9167e64d4d43aa37f1f826749
Aaron Holland <aaronholla@gmail.com>
2019-08-14 14:12:50 -0400

    fixed broken tests

--- spec/system/user_views_photo_spec.rb
+++ spec/system/user_views_photo_spec.rb
@@ -15,18 +15,37 @@ RSpec.describe "viewing photos", type: :system, js: true do
   end
 
   it "lists the photos" do
-    within "ul > li:first-of-type > a" do
+    first_list_image = page.all('ul > li:first-of-type a:first-of-type')[0]
+    within first_list_image do
       expect(page).to have_css("img[src*='#{url_for(Photo.last.image)}']")
     end
   end
   
-  it "has links to show page for photo" do
-    find("ul > li:first-of-type > a").click
+  it "should have links on photos that go to show page" do
+    first_list_image = page.all('ul > li:first-of-type a:first-of-type')[0]
+    first_list_image.click
     sleep 1
     expect(page.current_path).to eq(photo_path(Photo.last.id))
   end
 
-  xit "can scroll to top when scrolled down"
+  it "can scroll to top when scrolled down" do
+    # need more photos to test the scrolling.
+    sign_in(user)
+    visit root_path
+    4.times do
+      page.attach_file("#{Rails.root}/spec/support/assets/good/location.jpeg", visible: false) do
+        page.find('#image-upload-dropzone')
+      end
+      sleep 1
+      visit root_path
+    end
+    sign_out(user)
+    visit root_path
+    page.execute_script "window.scrollBy(0,10000)"
+    page.find('#scrollButton').click
+    sleep 1
+    expect(page.execute_script "return window.pageYOffset").to eq(0)
+  end
 
   def remove_uploaded_files
     FileUtils.rm_rf("#{Rails.root}/storage_test")
