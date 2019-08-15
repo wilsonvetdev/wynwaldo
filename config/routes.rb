aab60cef09ab04a16aa1459fb2aad47b6a8dd182
Aaron Holland <aaronholla@gmail.com>
2019-08-15 19:09:45 -0400

    added default url to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  default_url_options :host => "wynwaldo.herokuapp.com"
   get 'hello_world', to: 'hello_world#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root       'photos#index'
